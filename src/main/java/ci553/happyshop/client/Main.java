8842db579582e0e2368302b68ec797b41bb424c9
Arielli Ortega <a.ortega2@uni.brighton.ac.uk>
2026-01-09 20:43:19 +0000

    Clean Main.java - Simple Swing launcher only

--- src/main/java/ci553/happyshop/client/Main.java
+++ src/main/java/ci553/happyshop/client/Main.java
@@ -1,32 +1,22 @@
 package ci553.happyshop.client;
 
 import ci553.happyshop.client.shoppingcenter.ShoppingCenterView;
-import ci553.happyshop.dbAccess.DatabaseRW;
-import ci553.happyshop.dbAccess.DatabaseRWFactory;
-import ci553.happyshop.middle.OrderHub;
-
 import javax.swing.SwingUtilities;
 
 /**
- * Main Application Launcher - Fully Modernized Shopping Center
- * Launches ONLY the unified Shopping Center interface with Welcome Screen
- * All old HappyShop clients removed - clean, modern system
+ * Main Application Launcher - Modernized Shopping Center
+ * Launches ONLY the unified Shopping Center interface
+ * All old clients have been removed
  *
- * @version 5.0 - Complete Modernization
+ * @version 4.0 - Fully Modernized
  * @author Arielli Ortega, University of Brighton
  */
 public class Main {
 
     public static void main(String[] args) {
-        // Launch Shopping Center using Swing
         SwingUtilities.invokeLater(() -> {
-            // Initialize backend components
-            DatabaseRW databaseRW = DatabaseRWFactory.createDatabaseRW();
-            OrderHub orderHub = OrderHub.getOrderHub();
-            orderHub.initializeOrderMap();
-            
-            // Launch the modernized Shopping Center with Welcome Screen
-            ShoppingCenterView shoppingCenter = new ShoppingCenterView(databaseRW, orderHub);
+            // Launch the modernized Shopping Center with all features integrated
+            ShoppingCenterView shoppingCenter = new ShoppingCenterView(null, null);
             shoppingCenter.setVisible(true);
         });
     }
